78725034a99a0af22c3a337826aa0d002668175d
Igor Todorovski <39890068+IgorTodorovskiIBM@users.noreply.github.com>
2024-12-13 13:48:06 -0500

    Update cicd-stable.groovy

--- cicd-stable.groovy
+++ cicd-stable.groovy
@@ -9,6 +9,6 @@ node('linux')
       userRemoteConfigs: [[url: 'https://github.com/zopencommunity/caddyport.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/caddyport.git'), string(name: 'PORT_DESCRIPTION', value: 'Fast and extensible multi-platform HTTP/1-2-3 web server with automatic HTTPS' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/caddyport.git'), string(name: 'PORT_DESCRIPTION', value: 'Fast and extensible multi-platform HTTP/1-2-3 web server with automatic HTTPS' ), string(name: 'BUILD_LINE', value: 'STABLE'), string(name: 'NODE_LABEL', value: "go_122" ) ]
   }
 }
